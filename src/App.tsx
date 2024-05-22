e4cc8b5cd0efaa9533b82c71f8697b67067533dd
soumakkdev <soumakkdutta@gmail.com>
2024-05-22 21:56:34 +0530

    name updates

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 	const [color, setColor] = useColor('#74a1d4')
 	return (
 		<div className="max-w-6xl mx-auto p-5">
-			<h1 className="text-center text-4xl mb-8 font-medium">Color Picker</h1>
+			<h1 className="text-center text-4xl mb-8 font-medium">Hue Picker</h1>
 			<div className="grid grid-cols-2 gap-4">
 				<div className="">
 					<ColorPicker height={400} color={color} onChange={setColor} hideInput />
